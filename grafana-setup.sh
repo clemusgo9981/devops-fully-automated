f57f7e5e372a3662c8b9afc03da57fa3830c442e
Carlos Lemus <inter.milan8905@gmail.com>
2023-08-17 23:38:15 -0600

    Update grafana-setup.sh

--- grafana-setup.sh
+++ grafana-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Hardware requirements: Ubuntu 20.04 instance with mimum t2.micro type instance & port 3000 (grafana), 9100 (node-exporter) should be allowed on the security groups
+# Hardware requirements: Ubuntu 20.04 iVM & port 3000 (grafana), 9100 (node-exporter) should be allowed at firewall level.
 sudo apt-get install -y adduser libfontconfig1
 sudo wget https://dl.grafana.com/oss/release/grafana_7.3.4_amd64.deb
 sudo dpkg -i grafana_7.3.4_amd64.deb
@@ -17,7 +17,7 @@ sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/node_export
 rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
 
 # setup the node-exporter dependencies
-sudo git clone -b installations https://github.com/cvamsikrishna11/devops-fully-automated.git /tmp/devops-fully-automated
+sudo git clone -b installations https://github.com/clemusgo9981/devops-fully-automated.git /tmp/devops-fully-automated
 sudo cp /tmp/devops-fully-automated/prometheus-setup-dependencies/node-exporter.service /etc/systemd/system/node-exporter.service
 
 sudo systemctl daemon-reload
