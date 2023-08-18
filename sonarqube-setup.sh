b8ebe1ae191fb90cfab4dd16a3fc56516f959892
Carlos Lemus <inter.milan8905@gmail.com>
2023-08-18 01:08:27 -0600

    Update sonarqube-setup.sh

--- sonarqube-setup.sh
+++ sonarqube-setup.sh
@@ -43,7 +43,7 @@ java -version
 sudo apt update
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
 
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs) -pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 sudo apt install postgresql postgresql-contrib -y
 sudo -u postgres psql -c "SELECT version();"
 sudo systemctl enable postgresql.service
