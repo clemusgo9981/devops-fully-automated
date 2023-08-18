23022122319f4af60a8d283f232d3155c2b1fe47
Carlos Lemus <inter.milan8905@gmail.com>
2023-08-17 22:25:40 -0600

    Update jenkins-maven-ansible-setup.sh

--- jenkins-maven-ansible-setup.sh
+++ jenkins-maven-ansible-setup.sh
@@ -1,49 +1,41 @@
 #!/bin/bash
 # Hardware requirements: port 8080(jenkins), 9100 (node-exporter) should be allowed at firewall level
+
+# Update repositories
+sudo apt update && sudo apt upgrade -y
+
 # Installing Jenkins
-sudo apt update –y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key # Note: Refer this link to change this key line frequently https://pkg.jenkins.io/redhat-stable/
-sudo yum upgrade
-sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install jenkins -y
-sudo echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+sudo apt install wget openjdk-11-jdk -y
+sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+echo "deb https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list
+sudo apt update
+sudo apt install jenkins -y
+echo "jenkins ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
 # Installing Git
-sudo yum install git -y
+sudo apt install git -y
 
-# Installing maven - commented out as usage of tools explanation is required.
-# sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
-# sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-# sudo yum install -y apache-maven
-
-# Java installation
-# sudo yum install java-1.8.0-openjdk -y
-# sudo amazon-linux-extras install java-openjdk11 -y
+# Java installation (Java 11 is already installed above for Jenkins)
 
 # Installing Ansible
-sudo amazon-linux-extras install ansible2 -y
-sudo yum install python-pip -y
-sudo pip install boto3
-sudo useradd ansadmin
-sudo echo ansadmin:ansadmin | chpasswd
+sudo apt install ansible python3-pip -y
+sudo useradd ansible
+echo ansible:ansible | sudo chpasswd
 sudo sed -i "s/.*#host_key_checking = False/host_key_checking = False/g" /etc/ansible/ansible.cfg
-sudo sed -i "s/.*#enable_plugins = host_list, virtualbox, yaml, constructed/enable_plugins = aws_ec2/g" /etc/ansible/ansible.cfg
-sudo ansible-galaxy collection install amazon.aws
+sudo sed -i "s/.*#enable_plugins = host_list, virtualbox, yaml, constructed/enable_plugins = vmware_vm_inventory/g" /etc/ansible/ansible.cfg
+sudo ansible-galaxy collection install
 
 # node-exporter installations
 sudo useradd --no-create-home node_exporter
-
 wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
 tar xzf node_exporter-1.0.1.linux-amd64.tar.gz
 sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/node_exporter
 rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
 
 # setup the node-exporter dependencies
-sudo yum install git -y
-sudo git clone -b installations https://github.com/cvamsikrishna11/devops-fully-automated.git /tmp/devops-fully-automated
+git clone -b installations https://github.com/cvamsikrishna11/devops-fully-automated.git /tmp/devops-fully-automated
 sudo cp /tmp/devops-fully-automated/prometheus-setup-dependencies/node-exporter.service /etc/systemd/system/node-exporter.service
 
 sudo systemctl daemon-reload
@@ -51,7 +43,7 @@ sudo systemctl enable node-exporter
 sudo systemctl start node-exporter
 sudo systemctl status node-exporter
 
-# Setup terraform
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
+# Setup Terraform
+sudo apt install -y software-properties-common
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt update && sudo apt install terraform -y
