d5364ca32b73184473ebfe3f45fb0c6859df3624
Carlos Lemus <inter.milan8905@gmail.com>
2024-05-16 22:34:35 -0600

    Update jenkins-maven-ansible-setup.sh

--- jenkins-maven-ansible-setup.sh
+++ jenkins-maven-ansible-setup.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-# Hardware requirements: port 8080(jenkins), 9100 (node-exporter) should be allowed at firewall level
-
-# Update repositories
-sudo apt update && sudo apt upgrade -y
-
+# Hardware requirements: Amazon Linux 2 with mimum t2.medium type instance & port 8080(jenkins), 9100 (node-exporter) should be allowed on the security groups
+#Note: Dont use the latest EC2 Linux of 2023 as it doesnt have amazon-linux-extras which was used in the Jenkins installation as per the old AMI.
 # Installing Jenkins
-sudo apt install wget openjdk-11-jdk -y
-sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-echo "deb https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list
-sudo apt update
-sudo apt install jenkins -y
-echo "jenkins ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key # Note: Refer this link to change this key line frequently https://pkg.jenkins.io/redhat-stable/
+sudo yum upgrade
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install jenkins -y
+sudo echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
@@ -23,8 +21,8 @@ sudo apt install git -y
 sudo amazon-linux-extras install ansible2 -y
 sudo yum install python-pip -y
 sudo pip install boto3
-sudo useradd ansible
-echo ansible:ansible | sudo chpasswd
+sudo useradd ansadmin
+sudo echo ansadmin:ansadmin | chpasswd
 sudo sed -i "s/.*#host_key_checking = False/host_key_checking = False/g" /etc/ansible/ansible.cfg
 sudo sed -i "s/.*#enable_plugins = host_list, virtualbox, yaml, constructed/enable_plugins = aws_ec2/g" /etc/ansible/ansible.cfg
 sudo ansible-galaxy collection install amazon.aws
@@ -37,10 +35,16 @@ sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/node_export
 rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
 
 # setup the node-exporter dependencies
-git clone -b installations https://github.com/clemusgo9981/devops-fully-automated.git /tmp/devops-fully-automated
+sudo yum install -y
+sudo git clone -b installations https://github.com/clemusgo9981/devops-fully-automated.git /tmp/devops-fully-automated
 sudo cp /tmp/devops-fully-automated/prometheus-setup-dependencies/node-exporter.service /etc/systemd/system/node-exporter.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable node-exporter
 sudo systemctl start node-exporter
 sudo systemctl status node-exporter
+
+# Setup terraform
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
