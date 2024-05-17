894dc1452b2c1a345c3a1db1e076b9e8bbee9a6a
Carlos Lemus <inter.milan8905@gmail.com>
2024-05-16 23:00:43 -0600

    Update deployment-servers-setup.sh

--- deployment-servers-setup.sh
+++ deployment-servers-setup.sh
@@ -19,9 +19,9 @@ sudo systemctl start tomcat
 sudo useradd --no-create-home node_exporter
 
 wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
-tar xzf node_exporter-1.0.1.linux-amd64.tar.gz
+sudo tar xzf node_exporter-1.0.1.linux-amd64.tar.gz
 sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/node_exporter
-rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
+sudo rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
 
 # setup the node-exporter dependencies
 sudo yum install git -y
